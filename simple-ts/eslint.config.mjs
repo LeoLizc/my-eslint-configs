82d19d34c3e6deae6d67bca89951126ec92a801c
LeoLizc <leolizcanop@gmail.com>
2024-10-19 17:30:02 -0500

    second commit

---
+++ simple-ts/eslint.config.mjs
@@ -0,0 +1,49 @@
+import pluginJs from '@eslint/js';
+import auto from 'eslint-config-canonical/configurations/auto.js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default [
+  ...auto,
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+      '@typescript-eslint/lines-between-class-members': [
+        'error',
+        'always',
+        { exceptAfterSingleLine: true },
+      ],
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          format: ['camelCase', 'UPPER_CASE'],
+          leadingUnderscore: 'allow',
+          selector: 'variable',
+        },
+        {
+          format: ['camelCase'],
+          leadingUnderscore: 'allow',
+          selector: 'function',
+        },
+        {
+          format: ['PascalCase'],
+          selector: 'typeLike',
+        },
+      ],
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+      'no-console': 'warn',
+      'no-param-reassign': 'warn',
+    },
+  },
+];
